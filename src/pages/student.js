69ea04c09113c87ea90d89bb28fc5eb2cfebc44c
jayrajmeh <141761329+jayrajmeh@users.noreply.github.com>
2024-04-24 22:12:51 +0530

    set the label

--- src/pages/student.js
+++ src/pages/student.js
@@ -49,8 +49,12 @@ function HomePage() {
             <div className="school-details">
               <h3>{school.surname ? school.surname+" " + school.studentname+" " +school.fathername :"Add Student details Click here"}</h3>
               <h3>{moment(school.dateofbirth).format('DD-MM-YYYY')}</h3>
-              {school.status>0?<div style={{ backgroundColor: 'green', color: 'white', padding: '10px', marginTop: '10px' }}>
+              {school.status==1?<div style={{ backgroundColor: 'green', color: 'white', padding: '10px', marginTop: '10px' }}>
                  Your form submitted ,Thank You
+            </div>:school.status==2?<div style={{ backgroundColor: 'red', color: 'white', padding: '10px', marginTop: '10px' }}>
+                 Please Verify Your I-Card
+            </div>:(school.status==3 || school.status==4)?<div style={{ backgroundColor: 'green', color: 'white', padding: '10px', marginTop: '10px' }}>
+                 Thank You Your Feedback!
             </div>:null}
               {/* <p>{school.location}</p> */}
             </div>
